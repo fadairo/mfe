f1d1fa2fefbfce5442ab416ad18c39df0940e534
fadairo <andrew.fadairo@gmail.com>
2023-03-22 22:25:07 +0000

    Correcting name from app.js to App.js

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,3 +1,4 @@
+//Correcting name from app.js to App.js 
 import React from 'react';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
